c4a6aca86d1b533df1d147d48c4d1a9173ea88a4
Dixon Ramey <dixonrameyedu@gmail.com>
2024-05-30 19:07:45 -0400

    Start of Arrays

---
+++ src/Arrays/arrays.py
@@ -0,0 +1,10 @@
+
+
+#Arrays are a datastructure that hold elements at indexed positions
+#They are one of the simplest datastructures and are comparable to a list
+#In python arrays also act like a stack where you can pop from the front
+#
+
+
+
+
